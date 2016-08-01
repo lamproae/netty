1419ffa20bcbbab0afe08be8612ca7bc4273de28
Norman Maurer <norman_maurer@apple.com>
2016-08-01 11:21:08 +0200

    Port SendBufferPooled from netty 3.10 and use it if non pooled ByteBufAllocator is used.
    Motivation:
    
    At the moment we use ByteBufUtil.threadLocalBuffer() when we need to allocate a direct ByteBuf to write something to the underlying socket. This can cause a lot of ByteBuf instance to be pooled in the Recycler. Especially if the user calls a lot of write(...) and then only one flush() to use gathering writes. This is not optimial.
    
    Modification:
    
    Port SendBufferPool from 3.10 and use it for allocate direct buffers which can be used to write things to the socket.
    
    Result:
    
    Less resource usage and less instances in the Recycler.

--- transport/src/main/java/io/netty/channel/nio/NioEventLoop.java
+++ transport/src/main/java/io/netty/channel/nio/NioEventLoop.java
@@ -20,6 +20,7 @@ import io.netty.channel.ChannelException;
 import io.netty.channel.EventLoop;
 import io.netty.channel.EventLoopException;
 import io.netty.channel.SelectStrategy;
+import io.netty.channel.SendDirectByteBufPool;
 import io.netty.channel.SingleThreadEventLoop;
 import io.netty.util.IntSupplier;
 import io.netty.util.concurrent.RejectedExecutionHandler;
@@ -107,10 +108,13 @@ public final class NioEventLoop extends SingleThreadEventLoop {
         }
     }
 
+    final SendDirectByteBufPool bufferPool = new SendDirectByteBufPool();
+
     /**
      * The NIO {@link Selector}.
      */
     Selector selector;
+
     private SelectedSelectionKeySet selectedKeys;
 
     private final SelectorProvider provider;
@@ -435,6 +439,8 @@ public final class NioEventLoop extends SingleThreadEventLoop {
             selector.close();
         } catch (IOException e) {
             logger.warn("Failed to close a selector.", e);
+        } finally {
+            bufferPool.release();
         }
     }
 
